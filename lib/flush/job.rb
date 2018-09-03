24241872096db7a68d5eeb2d58ada08b185f932a
Marcelo Pélos <mpelos@gmail.com>
2018-09-03 17:40:45 -0300

    Fix bug when the job output is false.

--- lib/flush/job.rb
+++ lib/flush/job.rb
@@ -116,7 +116,7 @@ module Flush
 
     def output_payload
       expose_params.each_with_object({}) do |attr_name, acc|
-        attribute = output[attr_name.to_sym] || output[attr_name.to_s]
+        attribute = output.key?(attr_name.to_sym) ? output[attr_name.to_sym] : output[attr_name.to_s]
         acc[attr_name] = attribute
       end
     end
